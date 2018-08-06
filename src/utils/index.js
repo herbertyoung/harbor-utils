be04082c3f9bdfe5629b2fa0f7b26831f46e8c53
Harbor Young <yhb241@163.com>
2018-08-06 21:01:10 +0800

    Add convertToFormData

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,23 @@
 /**
+ * 将 Object 对象转变为 FormData 对象，可用于表单提交
+ * @param  {Object}   data  被转变的 Object 对象
+ * @return {FormData}       FormData 对象
+ */
+const convertToFormData = (data = {}) => {
+  return Object.entries(data).reduce((formData, [key, value]) => {
+    if (Array.isArray(value)) {
+      return value.reduce((formData, item) => {
+        formData.append(`${key}[]`, item);
+        return formData;
+      }, formData);
+    } else {
+      formData.append(key, value);
+    }
+    return formData;
+  }, new FormData());
+};
+
+/**
  * 从 Content-Disposition 首部获取文件名
  * @param  {string} disposition 首部字段值
  * @return {string}             文件名
@@ -18,5 +37,6 @@ const getFileNameFromContentDisposition = (disposition) => {
 };
 
 export default {
+  convertToFormData,
   getFileNameFromContentDisposition
 };
